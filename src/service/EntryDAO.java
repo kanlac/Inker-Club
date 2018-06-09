82208ae90844c8e2d08b7acf9905df252d9ef878
serfuse <serfuse@gmail.com>
2018-06-09 10:09:28 +0800

    Add getEntry method.

--- src/service/EntryDAO.java
+++ src/service/EntryDAO.java
@@ -1,21 +1,28 @@
 package service;
 
 import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanHandler;
+import org.apache.commons.dbutils.handlers.BeanListHandler;
 import presentation.model.Entry;
 import util.C3P0Utils;
 
 import java.sql.SQLException;
+import java.util.List;
 
 public class EntryDAO {
 
+    private static QueryRunner qr;
     private static int rows;
     private static String sql;
     private static Object[] params;
 
+    /**
+     * Updates.
+     */
+
     public static Boolean insert(Entry entry) {
 
-        QueryRunner qr = new QueryRunner();
-
+        qr = new QueryRunner();
         rows = 0;
         sql = "INSERT INTO entry (title, date, author, content) VALUES (?, ?, ?, ?)";
         params = new Object[] {
@@ -33,8 +40,7 @@ public class EntryDAO {
 
     public static Boolean update(Entry entry) {
 
-        QueryRunner qr = new QueryRunner();
-
+        qr = new QueryRunner();
         rows = 0;
         sql = "UPDATE books SET title = ?, content = ? WHERE e_id = ?";
         params = new Object[] {
@@ -52,8 +58,7 @@ public class EntryDAO {
 
     public static Boolean delete(Entry entry) {
 
-        QueryRunner qr = new QueryRunner();
-
+        qr = new QueryRunner();
         rows = 0;
         sql = "DELETE FROM entry WHERE e_id = ?";
         params = new Object[] {
@@ -67,6 +72,26 @@ public class EntryDAO {
         }
 
         return rows > 0;
+    }
+
+    /**
+     * Queries.
+     */
+
+    public static Entry getEntry(String id) {
+
+        qr = new QueryRunner();
+        sql = "SELECT * FROM entry WHERE e_id = ?";
+        params = new Object[] { id };
+        Entry rBean = new Entry();
+
+        try {
+            rBean = (Entry) qr.query(C3P0Utils.getConnection(), sql, new BeanHandler(Entry.class), params);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return rBean;
     }
 
 }
